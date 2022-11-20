1f11ab239be068dcdc4755bd519481c216df2f2e
unknown <ekat.sergsed@gmail.com>
2022-11-20 22:32:34 +0300

    hw1-2

---
+++ HW/hw1/hw1-2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a < b) 
+    Console.WriteLine($"max = {b}");
+else Console.WriteLine($"max = {a}");
+Console.ReadLine();
