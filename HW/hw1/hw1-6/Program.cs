d2703a692439ab2d952a8a25a2ac6b81fbfbf889
unknown <ekat.sergsed@gmail.com>
2022-11-20 22:37:54 +0300

    hw1-6

---
+++ HW/hw1/hw1-6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 6. Напишите программу, которая на вход принимает число и выдает, является ли число четным.
+
+Console.Write("Введите число ");
+double n = Convert.ToDouble(Console.ReadLine());
+double a = n % 2;
+
+if (a == 0) 
+    Console.WriteLine("да");
+else Console.WriteLine("нет");
+Console.ReadLine();
