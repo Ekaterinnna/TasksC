b843322bbffcbd2cd36f738a0c4355cce2b60cd4
unknown <ekat.sergsed@gmail.com>
2022-11-22 21:57:53 +0300

    hw2-13 is ready

---
+++ HW/hw2/hw2-13/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int a = n / 100;
+if (a == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    int k = 0;
+    int r = n;
+    while (r != 0)
+    {
+        r = r / 10;
+        k++;
+    }
+    Console.WriteLine($"Знаков в числе {k}");
+    if (k == 3)
+    {
+        int t = n % 10;
+        Console.WriteLine($"Третья цифра = {t}");
+    }
+    else
+    {
+        int p = k - 3;
+        int y = Convert.ToInt32(Math.Pow(10, p));
+        int t = (n / y) % 10;
+        Console.WriteLine($"Третья цифра = {t}");
+    }
+}
