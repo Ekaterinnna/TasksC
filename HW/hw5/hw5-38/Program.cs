a43296a2ae3e77abc8c4f240162e84fbb0107839
unknown <ekat.sergsed@gmail.com>
2022-11-27 00:41:10 +0300

    hw5-38 is ready

---
+++ HW/hw5/hw5-38/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// Методы задания массива
+
+int GetNumber(string text)
+{
+    Console.Write(text);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+double[] CreateArray(int count)
+{
+    return new double[count];
+}
+
+void Fill(double[] array)
+{
+    int size = array.Length;
+
+    int index = 0;
+    while (index < size)
+    {
+        array[index] = new Random().Next(0, 100);
+        index++;
+    }
+}
+
+string PrintGood(double[] numbers)
+{
+  int size = numbers.Length;
+  int i = 0;
+  string result = "[ ";
+
+  while (i < size)
+  {
+    result += ($"{numbers[i]} ");
+    i++;
+  }
+  return result + "]";
+}
+
+// метод - найти макс
+
+double FindMax(double[] arr)
+{
+    int len = arr.Length;
+    double max = arr[0];
+    for (int i = 1; i < len; i++)
+    {
+        if (max < arr[i])
+            max = arr[i];
+    }
+    return max;
+}
+
+// метод - найти мин
+
+double FindMin(double[] arr)
+{
+    int len = arr.Length;
+    double min = arr[0];
+    for (int i = 1; i < len; i++)
+    {
+        if (min > arr[i])
+            min = arr[i];
+    }
+    return min;
+}
+
+// посчитать разницу и вывести
+
+int count = GetNumber("Введите количество элементов в массиве: ");
+double[] array = CreateArray(count);
+Fill(array);
+Console.WriteLine(PrintGood(array));
+double max = FindMax(array);
+double min = FindMin(array);
+double res = max - min;
+Console.WriteLine(res);
