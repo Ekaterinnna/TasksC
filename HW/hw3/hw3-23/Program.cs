5a6be537ffa742039221e3c8a6c2bb04ea894b80
unknown <ekat.sergsed@gmail.com>
2022-11-27 13:55:43 +0300

    hw3-23 is ready

---
+++ HW/hw3/hw3-23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int GetNumber(string text)
+   {
+    Console.Write(text);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+   }
+
+int NumSquare(int a)
+{
+    int res = a*a;
+    return res;
+}
+
+int num = GetNumber("Введите число: ");
+for (int i = 1; i <= num; i++)
+{
+    int sq = NumSquare(i);
+    Console.Write($"{sq} ");
+}
