288b5e59e96f27c23f5e2bde5feb184f394a4a77
unknown <ekat.sergsed@gmail.com>
2022-12-01 21:57:20 +0300

    Start 58

---
+++ Sem/Sem0112/sem-58/Program.cs
@@ -0,0 +1,46 @@
+﻿// 58. Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить, что это невозможно (в случае, если матрица не квадратная).
+/*
+1. Создание массива
+2. Заполнение массива
+3. Вывод массива
+4. Метод создания нового перевернутого массива, если возможно
+*/
+
+int[,] Create(int a, int b)
+{
+  int[,] table = new int[a, b];
+  return table;
+}
+
+void Fill(int[,] tab)
+{
+    int row = tab.GetLength(0);
+    int col = tab.GetLength(1);
+    for (int i = 0; i < row; i++)
+      for (int j = 0; j < col; j++)
+    {
+        tab[i,j] = new Random().Next(0, 100);
+    }
+}
+
+void PrintTable(int[,] tab)
+{
+  int row = tab.GetLength(0);
+  int col = tab.GetLength(1);
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      Console.Write($"{tab[i, j],4}");
+    }
+    Console.WriteLine();
+  }
+}
+
+int a = 3;
+int b = 3;
+int[,] ourtable = Create(a,b);
+Fill(ourtable);
+Console.WriteLine();
+PrintTable(ourtable);
+Console.WriteLine();
