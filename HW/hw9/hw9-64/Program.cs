fd2ad70b5f4034bad6357decaf22dfe3189a7a8a
unknown <ekat.sergsed@gmail.com>
2022-12-04 22:33:37 +0300

    hw9-64 is ready

---
+++ HW/hw9/hw9-64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int GetNumber(string text)
+   {
+    Console.Write(text);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+   }
+
+void Printing(int N)
+{
+    Console.Write($"{N}, ");
+    if (N != 2) Printing(N-1);
+    else Console.Write("1");
+}
+
+int N = GetNumber("Введите число N = ");
+Printing(N);
